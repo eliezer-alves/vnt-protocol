a768dd9e53ee41fd14628a00291c0967c7622ed8
Eliezer Alves <eliezer.cardoso@venturus.org.br>
2022-01-18 20:53:02 -0300

    feat(vnt_protocol): criando metodo fill

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from vnt_protocol import VntProtocol
 if __name__ == '__main__':
-    msg = VntProtocol()
+    vnt_protocol = VntProtocol()
+    body = {
+        'id' : 'VNTELCA',
+        'conta' : 'FTI',
+        'idade' : 21,
+        'estado' : 'MG',
+        'curso' : 'CIENCIA DA COMPUTACAO',
+        'linguagem' : 'PHP',
+    }
+    
+    vnt_protocol.fill(body)
+
+    print(vnt_protocol.idade)
     # v = Ventureiro('VNTELCA', 'FTI', 21, 'MG', 'CIENCIA DA COMPUTACAO', 'PHP')
     # print(v.conta)
--- vnt_protocol.py
+++ vnt_protocol.py
@@ -2,6 +2,17 @@ from constants import ESTADOS, CONTAS, CURSOS, LINGUAGENS
 
 class VntProtocol:
 
-    def __init__(self):
-        print("iniciando classe de protocolo venturus")
-        print(ESTADOS, CONTAS, CURSOS, LINGUAGENS)
+    id = ''
+    conta = ''
+    idade = 5
+    estado = ''
+    curso = ''
+    linguagem = ''
+    
+    def fill(self, attributes):
+        self.id = attributes['id']
+        self.conta = attributes['conta']
+        self.idade = attributes['idade']
+        self.estado = attributes['estado']
+        self.curso = attributes['curso']
+        self.linguagem = attributes['linguagem']
