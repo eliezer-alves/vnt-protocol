1c53ea4a54c6f0f47a5a191c22157595045fb012
Eliezer Alves <eliezer.cardoso@venturus.org.br>
2022-01-18 20:24:00 -0300

    feat: criando estrutura base

---
+++ main.py
@@ -0,0 +1,5 @@
+from vnt_protocol import VntProtocol
+if __name__ == '__main__':
+    msg = VntProtocol()
+    # v = Ventureiro('VNTELCA', 'FTI', 21, 'MG', 'CIENCIA DA COMPUTACAO', 'PHP')
+    # print(v.conta)
---
+++ vnt_protocol.py
@@ -0,0 +1,7 @@
+from constants import ESTADOS, CONTAS, CURSOS, LINGUAGENS
+
+class VntProtocol:
+
+    def __init__(self):
+        print("iniciando classe de protocolo venturus")
+        print(ESTADOS, CONTAS, CURSOS, LINGUAGENS)
