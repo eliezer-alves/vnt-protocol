2e4c63dec9de33c3703b68a928dc540ff2fc0a2c
Eliezer Alves <eliezer.cardoso@venturus.org.br>
2022-01-19 13:13:28 -0300

    feat(vnt_protocol): criando funcionalidade de hidratar a classe com um hexadecimal

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from vnt_protocol import VntProtocol
 if __name__ == '__main__':
     vnt_protocol = VntProtocol()
-    body = {
-        'id' : 'VNTELCA',
-        'conta' : 'FTI',
-        'idade' : 21,
-        'estado' : 'MG',
-        'curso' : 'CIENCIA DA COMPUTACAO',
-        'linguagem' : 'PHP',
-    }
+    vnt_protocol.hydrate_with_hexa('766E747065616C348787')
+    print(vnt_protocol)
+    # print(vnt_protocol.binario[::-1])
     
-    vnt_protocol.fill(body)
+    
+    # body = {
+    #     'id' : 'VNTELCA',
+    #     'conta' : 'FTI',
+    #     'idade' : 21,
+    #     'estado' : 'MG',
+    #     'curso' : 'CIENCIA DA COMPUTACAO',
+    #     'linguagem' : 'PHP',
+    # }
+    
+    # vnt_protocol.fill(body)
 
-    print(vnt_protocol.idade)
     # v = Ventureiro('VNTELCA', 'FTI', 21, 'MG', 'CIENCIA DA COMPUTACAO', 'PHP')
     # print(v.conta)
--- vnt_protocol.py
+++ vnt_protocol.py
@@ -4,15 +4,56 @@ class VntProtocol:
 
     id = ''
     conta = ''
-    idade = 5
+    idade = 0
     estado = ''
     curso = ''
     linguagem = ''
+    binario = ''
     
-    def fill(self, attributes):
+    def __str__(self):
+        return f'\n## VntProtocol\n{{\n   id => {self.id},\n   conta => {self.conta},\n   idade => {self.idade},\n   estado => {self.estado},\n   curso => {self.curso},\n   linguagem => {self.linguagem}\n}}\n'
+
+    def hydrate(self, attributes):
         self.id = attributes['id']
         self.conta = attributes['conta']
         self.idade = attributes['idade']
         self.estado = attributes['estado']
         self.curso = attributes['curso']
         self.linguagem = attributes['linguagem']
+    
+    def hydrate_with_hexa(self, hexa):
+        binario = self.hexa_to_bin(hexa)[::-1]
+
+        self.linguagem = (binario[0:4])[::-1]
+        self.curso = (binario[4:8])[::-1]
+        self.estado = (binario[8:13])[::-1]
+        self.idade = (binario[13:20])[::-1]
+        self.conta = (binario[20:24])[::-1]
+        self.id = (binario[24::])[::-1]
+    
+    def encode(self):
+        encoded_data = "00000000000000000000000000000000000000000000000000000000000000000000000000000000"
+
+        return encoded_data
+
+    def decode(self, encoded_data):
+        binario = self.hexa_to_bin(encoded_data)
+
+        self.idade = format(self.idade, "d")
+
+    
+    def hexa_to_bin(self, hash):
+        binario = ''
+        for i in hash:
+            decimal = int(i,16)
+            binario += str(format(decimal, "b")).rjust(4, '0')
+        self.binario = binario
+        return binario
+        
+
+    def transmit(encoded_data):
+        return
+
+    def scan(self):
+        encoded_data = "00000000000000000000000000000000000000000000000000000000000000000000000000000000"
+        return encoded_data
