4fba7d5b5731d2b13b10d7bb4cf741b5bf6e4c34
eliezer <eliezer.c.alves2015@gmail.com>
2022-01-19 20:02:47 -0300

    feat(vnt_protocol): implementando estrutura para decodificar os atributos da classe uma vez que estao em binario

--- vnt_protocol.py
+++ vnt_protocol.py
@@ -23,33 +23,27 @@ class VntProtocol:
     
     def hydrate_with_hexa(self, hexa):
         binario = self.hexa_to_bin(hexa)[::-1]
-
-        self.linguagem = (binario[0:4])[::-1]
-        self.curso = (binario[4:8])[::-1]
-        self.estado = (binario[8:13])[::-1]
-        self.idade = (binario[13:20])[::-1]
-        self.conta = (binario[20:24])[::-1]
-        self.id = (binario[24::])[::-1]
+        
+        self.binario    = binario
+        self.linguagem  = (binario[0:4])[::-1]
+        self.curso      = (binario[4:8])[::-1]
+        self.estado     = (binario[8:13])[::-1]
+        self.idade      = (binario[13:20])[::-1]
+        self.conta      = (binario[20:24])[::-1]
+        self.id         = (binario[24::])[::-1]
     
     def encode(self):
         encoded_data = "00000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
         return encoded_data
 
-    def decode(self, encoded_data):
-        binario = self.hexa_to_bin(encoded_data)
-
-        self.idade = format(self.idade, "d")
-
-    
-    def hexa_to_bin(self, hash):
-        binario = ''
-        for i in hash:
-            decimal = int(i,16)
-            binario += str(format(decimal, "b")).rjust(4, '0')
-        self.binario = binario
-        return binario
-        
+    def decode(self):
+        self.linguagem  = LINGUAGENS[self.bin_to_dec(self.linguagem)]
+        self.curso      = CURSOS[self.bin_to_dec(self.curso)]
+        self.estado     = ESTADOS[self.bin_to_dec(self.estado)]
+        self.conta      = CONTAS[self.bin_to_dec(self.conta)]
+        self.idade      = self.bin_to_dec(self.idade)
+        self.id         = self.bin_to_dec(self.id)
 
     def transmit(encoded_data):
         return
@@ -57,3 +51,25 @@ class VntProtocol:
     def scan(self):
         encoded_data = "00000000000000000000000000000000000000000000000000000000000000000000000000000000"
         return encoded_data
+
+    
+    def hexa_to_bin(self, hash):
+        binario = ''
+        for i in hash:
+            decimal = int(i,16)
+            binario += str(format(decimal, "b")).rjust(4, '0')
+        
+        return binario
+
+    def bin_to_dec(self, binario):
+        decimal = 0
+        cont = 1
+        print(binario)
+        for i in reversed(binario):
+            decimal += int(i)*cont
+            if((cont%2) == 0):                
+                cont*=2
+            else:
+                cont+=1
+
+        return decimal
