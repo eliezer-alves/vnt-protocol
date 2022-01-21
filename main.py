987f669919daac9b847d42c78a5f26b685089ff6
eliezer <eliezer.c.alves2015@gmail.com>
2022-01-21 01:17:01 -0300

    test(main): fazendo teste de decode com um hexadecimal pronto

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
 from vnt_protocol import VntProtocol
 if __name__ == '__main__':
+    print("\n\n\n#####__________________________________________________#####\n")
     vnt_protocol = VntProtocol()
     
-    # vnt_protocol.hydrate_with_hexa('766E747065616C348787')
-    # vnt_protocol.decode()
-    # print(vnt_protocol)
-    
-    # vnt_protocol.hydrate_with_hexa('766e7466617261d17456')
-    # vnt_protocol.decode()
-    # print(vnt_protocol)
-    
-    
-    body = {
-        'id' : 'VNTELCA',
-        'conta' : 'FTI',
-        'idade' : 21,
-        'estado' : 'MG',
-        'curso' : 'CIENCIA DA COMPUTACAO',
-        'linguagem' : 'PHP',
-    }
-    
-    vnt_protocol.hydrate(body)
-    # print(vnt_protocol)
-
-    vnt_protocol.encode()
+    vnt_protocol.hydrate_with_hexa('766e7467756963423999')
+    vnt_protocol.decode()
     print(vnt_protocol.binario)
     print(vnt_protocol.hexa)
-
-    vnt_protocol.hydrate_with_hexa(vnt_protocol.hexa)
-    vnt_protocol.decode()
     print(vnt_protocol)
-
-    # v = Ventureiro('VNTELCA', 'FTI', 21, 'MG', 'CIENCIA DA COMPUTACAO', 'PHP')
-    # print(v.conta)
