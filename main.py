f9d7fccecbae4b2b47b5d610413e0a0470d377b9
eliezer <eliezer.c.alves2015@gmail.com>
2022-01-21 01:18:30 -0300

    test(main): fazendo teste de encode com uma estrutura de dados

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ if __name__ == '__main__':
     print("\n\n\n#####__________________________________________________#####\n")
     vnt_protocol = VntProtocol()
     
-    vnt_protocol.hydrate_with_hexa('766e7467756963423999')
-    vnt_protocol.decode()
+    # vnt_protocol.hydrate_with_hexa('766e7467756963423999')
+    # vnt_protocol.decode()
+    # print(vnt_protocol.binario)
+    # print(vnt_protocol.hexa)
+    # print(vnt_protocol)
+
+    body = {
+        'id' : 'vntpeal',
+        'conta' : 'COMPAL',
+        'idade' : 36,
+        'estado' : 'DF',
+        'curso' : 'ENGENHARIA MECATRONICA',
+        'linguagem' : 'PYTHON',
+    }
+    
+    vnt_protocol.hydrate(body)
+    vnt_protocol.encode()
     print(vnt_protocol.binario)
     print(vnt_protocol.hexa)
     print(vnt_protocol)
